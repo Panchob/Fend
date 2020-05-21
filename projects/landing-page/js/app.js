0282277d0f7d2468a90b8153fe4834b75843717a
Francis <bilodeaurobillardfrancis@gmail.com>
2020-05-21 08:35:16 -0400

    feat: functions to add sections and add to navbar

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -13,28 +13,52 @@
  * 
 */
 
-/**
- * Define Global Variables
- * 
-*/
 
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+function countNbOfSections(){
+    return document.getElementsByTagName("section").length;
+}
+
+
+function addSections(nbToAdd, currentNbOfSections){
+    let contentToAdd ="";
+    const mainSection = document.querySelector("main");
+    currentNbOfSections ++;
+
+    for (let i = 0; i < nbToAdd; i++){
+        const section = 
+        `<section id="section${currentNbOfSections}" data-nav="Section ${currentNbOfSections}" >
+            <div class="landing__container">
+                <h2>Section ${currentNbOfSections}</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
+    
+                <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+            </div>
+        </section>`;
+        contentToAdd += section;
+        currentNbOfSections++;
+    }
+    mainSection.insertAdjacentHTML("beforeend", contentToAdd);
+}
+
+function populateNavBar(nbOfSections){
+    const navbar = document.getElementById("navbar__list")
+    let listElements = ""
+
+    for (let i = 0; i <= nbOfSections; i++){
+        list = `<li class=menu__link>Section ${i}</li>`;
+        listElements += list;
+    }
+    navbar.insertAdjacentHTML("afterbegin", listElements);
+}
 
 
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+const currentNbOfSections = countNbOfSections();
+const nbOfSectionsToAdd = 3;
 
-// build the nav
-
+addSections(nbOfSectionsToAdd, currentNbOfSections);
+populateNavBar(nbOfSectionsToAdd + currentNbOfSections);
 
 // Add class 'active' to section when near top of viewport
 
@@ -53,5 +77,3 @@
 // Scroll to section on link click
 
 // Set sections as active
-
-
